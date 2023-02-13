192669a0c036d210f3f32dd7da2ebda41437317f
Dwayne R <rdwayne876@gmail.com>
2023-02-13 07:16:12 -0500

    tailwind install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatwindowComponent } from './chatwindow/chatwindow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatwindowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chatwindow/chatwindow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatwindow',
+  templateUrl: './chatwindow.component.html',
+  styleUrls: ['./chatwindow.component.scss']
+})
+export class ChatwindowComponent {
+
+}
